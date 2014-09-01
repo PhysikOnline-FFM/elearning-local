c5ff60edfd4b6b205ecd33f45aa1cda48d0d3e2a
Elearning Web Account <elearning-www@th.physik.uni-frankfurt.de>
2014-09-01 11:59:53 +0200

    Aktualisierung des Repos

--- Customizing/global/skin/po-v3.0/js/hauptseite.js
+++ Customizing/global/skin/po-v3.0/js/hauptseite.js
@@ -8,8 +8,8 @@
  *
  **/
 
-if(!window.po) window.po = {}; // PhysikOnline Namespace
-po.hauptseite = {}; // aHauptseite-Namensraum
+if(!window.po) window.po = {}; 	// PhysikOnline Namespace
+po.hauptseite = {}; 			// Hauptseite-Namensraum
 
 po.hauptseite.setup = function() {
 	// Loginbox auf Startseite funktionsfaehig machen, POTT #643
@@ -51,61 +51,40 @@ po.hauptseite.setup = function() {
 
 po.hauptseite.contentManipulation = function() {
 	// More-Button in Willkommens-Box
-	$('div#more a.more').click(function(){
-		$(this).remove();
-		$('div#po3-hauptseite-willkommen div.more').slideToggle();
+	$('div#more-btn a').click(function(){
+		$('div#more-text').slideToggle(300, function(){
+			if ($(this).is(':visible'))
+				$('div#more-btn a').text('weniger');
+			else
+				$('div#more-btn a').text('mehr');
+		});
 	});
 	
 	// Entferne Willkommen+Login-Box,
 	// um sie ggf. später außerhalb des regulären Content-Bereichs wieder einzufügen.
 	var block = $("#po3-hauptseite-top").detach();
 	
-	// Füge PodcastWiki Zeile ein
-	$("#po-hauptseite table.ilContainerBlock").append("<tr id='item_row_podcastwiki' class='ilCLIRow2'><td class='ilCLI'><div id='po3-podcastwiki' class='ilContainerListItemOuter'><div><a href='http://podcast-wiki.physik.uni-frankfurt.de/'><img width='22' src='/local/logos/podcast-goethe-klein.png' title='Symbol Kategorie' alt='' style='position: absolute;  padding: 0px;'></img></a></div><div style='margin: 0px 0px 0px 25px; padding: 0px;'><div class='il_ContainerListItem'><div class='po3-itemHeader'><div style='float:left;padding-bottom: 5px'><h4 class='il_ContainerItemTitle'><a class='il_ContainerItemTitle' href='http://podcast-wiki.physik.uni-frankfurt.de/'>  Podcast Wiki Physik</a></h4></div></div><div class='il_Description' style='clear:both; zoom:1;'>PodcastWiki ist ein studentisches Videoprojekt, das physikalische Experimente vorführt, Vorlesungsinhalte erklärt oder Arbeitsgruppen des Fachbereichs Physik der Goethe-Universität vorstellt.</div><div style='clear:both;'></div></div></div></div></td></tr>");
-	
 	if ($("#po3-anonymous-user").length !== 0){
 		// Wenn nicht angemeldet
 		$("#mainspacekeeper div.il_HeaderInner").append(block);
-		
-		/* Deaktiviert mit neuer Startseite 05.11.2013 - Philip
-		// Anpassung der Kategorie-Liste auf der Startseite
-		var rows = "#item_row_1361, #item_row_90, #item_row_1393, #item_row_75, #item_row_76";
-		// Blende obige Listenpunkte aus
-		$(rows).hide();
-		// Füge "Alle anzeigen" hinzu
-		$("<tr>")
-			.addClass("ilCLIRow2")
-			.attr("id", "po3-showAllRows")
-			.append(
-				$("<td>")
-					.css({"text-align": "center"})
-					.append(
-						$("<a>")
-							.css({"display": "block"})
-							.text("Alle anzeigen")
-							.click(function(){
-								$(rows).show();
-								$("#po3-showAllRows").hide();
-							})
-					)
-				)
-			.appendTo("#po-hauptseite table.ilContainerBlock");
-		*/
 	}
+	
+	// Füge PodcastWiki Zeile ein
+	$("#po-hauptseite table.ilContainerBlock").append("<tr id='item_row_podcastwiki' class='ilCLIRow2'><td class='ilCLI'><div id='po3-podcastwiki' class='ilContainerListItemOuter'><div><a href='http://podcast-wiki.physik.uni-frankfurt.de/'><img width='22' src='/local/logos/podcast-goethe-klein.png' title='Symbol Kategorie' alt='' style='position: absolute;  padding: 0px;'></img></a></div><div style='margin: 0px 0px 0px 25px; padding: 0px;'><div class='il_ContainerListItem'><div class='po3-itemHeader'><div style='float:left;padding-bottom: 5px'><h4 class='il_ContainerItemTitle'><a class='il_ContainerItemTitle' href='http://podcast-wiki.physik.uni-frankfurt.de/'>  Podcast Wiki Physik</a></h4></div></div><div class='il_Description' style='clear:both; zoom:1;'>PodcastWiki ist ein studentisches Videoprojekt, das physikalische Experimente vorführt, Vorlesungsinhalte erklärt oder Arbeitsgruppen des Fachbereichs Physik der Goethe-Universität vorstellt.</div><div style='clear:both;'></div></div></div></div></td></tr>");
 	
 	// Metro, Kachel, Tile - Startseite im frischen Design
 	if ($("#po3-hauptseite-tile").length){
 		// Block mit Kacheln aufgreifen um ihn später an anderer Stelle einzublenden
 		var tile = $("#po3-hauptseite-tile").detach();
 		// Gewöhnliche Kategorieliste ausblenden
-		$("#po3-hauptseite-list").closest('.ilTable').hide();
+		$("div.ilTable, div.il_Header div.il_Tab").hide();
 		
-		$("#mainspacekeeper div.il_HeaderInner").append(tile.show());
+		$("#mainspacekeeper div.il_HeaderOuter").append(tile.show());
 		
 		// Schalter aktivieren
 		$("#po3-tile-schalter").click(function(){
-			$("#po3-hauptseite-tile").hide(500);
-			$("#po3-hauptseite-list").closest('.ilTable').show(500);
+			$("#po3-hauptseite-tile").hide(700);
+			$("div.ilTable, div.il_Header div.il_Tab").show(700);
 		});
 	}
 };
